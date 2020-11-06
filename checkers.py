fb5a26cf34212d99a40b39ca5685494a9d8d74b3
Camilo Ramirez <camilorg@kth.se>
2020-11-06 18:41:21 +0100

    improved the checkers messages and improved some descriptions

--- checkers.py
+++ checkers.py
@@ -1,20 +1,20 @@
 import numpy as np
 import sys
-
+from IPython.display import display, Markdown, Latex
 
 def checker_crf(value, discount_rate, year):
     crf = (1 + discount_rate) ** -year
-    if len(value) != len(year):
-        return "The length of your array should be the same as the number of years."
     if isinstance(value, np.ndarray):
-        if sum(crf == value) == len(crf):
-            return "Your answer is correct!"
+        if len(value) != len(year):
+            return print("The length of your array should be the same as the number of years.")
+        elif sum(crf == value) == len(crf):
+            return print("Your answer is correct!")
         else:
-            return "The values provided are not correct. They should be:\n{}".format(
+            return print("The values provided are not correct. They should be:\n{}".format(
                 crf
-            )
+            ))
     else:
-        return "The argument `value` needs to be `np.array()`"
+        return display(Markdown("The argument `crf` needs to be of type `np.array()`"))
 
 
 def checker_lcoe(
@@ -26,15 +26,48 @@ def checker_lcoe(
     technical_lifetime,
 ):
     try:
-        year = np.arange(technical_lifetime)
-        crf = (1 + discount_rate) ** -year
-        lcoe = (capital_costs + sum(crf * operational_costs)) / sum(
-            crf * electricity_generation
-        )
-        if value == lcoe:
-            return "Your answer is correct!"
+        if isinstance(value, float):
+            year = np.arange(technical_lifetime)
+            crf = (1 + discount_rate) ** -year
+            lcoe = (capital_costs + sum(crf * operational_costs)) / sum(
+                crf * electricity_generation
+            )
+            if value == lcoe:
+                return print("Your answer is correct!")
+            else:
+                return print("That is not right, try again!")
         else:
-            return "That is not right, try again!"
+            return display(Markdown("The argument `lcoe` needs to be of type `float`"))
     except:
-        return "Unexpected error: {}".format(sys.exc_info()[0])
+        return print("Unexpected error: {}".format(sys.exc_info()[0]))
+        raise
+        
+        
+def checker_parameters(
+    el_gen, 
+    op_costs, 
+    capacity, 
+    load_factor, 
+    fixed_om_cost,
+    fuel_price,
+    efficiency
+):
+    electricity_generation = capacity * load_factor * 8760 # in kWh
+    annual_fixed_om_cost = capacity * fixed_om_cost # in €
+    annual_variable_om_cost = fuel_price * electricity_generation / efficiency # in €
+    operational_costs = annual_fixed_om_cost + annual_variable_om_cost # in €
+    
+    try:
+        if el_gen == electricity_generation:
+            str_out = 'Your electricity generation is correct!\n'
+        else:
+            str_out = f'Your electricity generation is not right... it should be {electricity_generation}\n'
+        if op_costs == operational_costs:
+            str_out += 'Your operational costs are correct!'
+        else:
+            str_out += f'Your operational costs are not right... they should be {operational_costs}'
+        return print(str_out)
+    except:
+        return print("Unexpected error: {}".format(sys.exc_info()[0]), 
+                     'Make sure your electricity generation and operational values are of type float')
         raise
